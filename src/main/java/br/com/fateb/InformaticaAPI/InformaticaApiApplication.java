d092521a092b77cd0e9b399091b0f6b4aa75be2e
Marcos Albano <marcosaalbanojunior@gmail.com>
2024-03-18 21:53:18 -0300

    priemiro commit

---
+++ src/main/java/br/com/fateb/InformaticaAPI/InformaticaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fateb.InformaticaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformaticaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformaticaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fateb/InformaticaAPI/InformaticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fateb.InformaticaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformaticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
